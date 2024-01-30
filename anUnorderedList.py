0e77b652d653b796ec7fde3c1ac1d7326329cee9
Alex <nayzinminlwin22@gmail.com>
2024-01-30 22:37:28 +0630

    Finally done with UnorderedList

--- anUnorderedList.py
+++ anUnorderedList.py
@@ -1,5 +1,7 @@
 from node import Node
 
+# Head->1->99->66->None
+
 class UnorderedList:
 	def __init__(self):
 		self.head = None
@@ -11,6 +13,37 @@ class UnorderedList:
 		temp = Node(Val)
 		temp.set_next(self.head)
 		self.head = temp
+		print("Added",Val)
+
+	def insert(self,pos,Val):
+
+		temp = Node(Val)
+		current = self.head
+		previous = None
+
+		for x in range(pos):
+			previous = current
+			current = current.get_next()
+
+		temp.set_next(current)
+		previous.set_next(temp)
+		print("Completely inserted",Val,"into index :",pos)
+
+	def append(self,Val):
+		temp = Node(Val)
+		current = self.head
+
+		while current != None:
+			# print("Incoming into while!")
+			if current.get_next() == None:
+				# print("Finally into if clause")
+				current.set_next(temp)
+				temp.set_next(None)
+				break
+
+			else:
+				current = current.get_next()
+		print("Successfully appened",Val)
 
 	def listSize(self):
 		current = self.head
@@ -55,21 +88,20 @@ class UnorderedList:
 		current = self.head
 		previous = None
 
-		while current!= None :
-			previous = current
-			current = current.get_next()
-			# print("Now i am at",self.index(current.get_data()))
-			# print("previous data is at",self.index(previous.get_data()))
+		if current!= None :
+			count = self.listSize()
 
-		# previous.set_next(None)
-		# previous.head = None
-		# previous.set_next(current)
-		# self.delete(previous.get_data())
-		if previous == None :
-			self.head = current.get_next()
-		else: 
-			previous.set_next(None)
-
+			for x in range(count-1):
+				previous = current
+				current = current.get_next()
+				# print("Now i am at",self.index(current.get_data()))
+				# print("previous data is at",self.index(previous.get_data()))
+	
+		# Head->1->99->66->None
+		# print("current head is",current.get_data())
+		# print("previous head is ",previous.get_data())
+		previous.set_next(None)
+		print("Successfully popped the last data from list.")
 
 	def delete(self,val):
 
@@ -90,23 +122,32 @@ class UnorderedList:
 			else: 
 				previous.set_next(current.get_next())
 
- # append,insert,pop
+		print("Successfully deleted",val,"from the list.")
+
 
 myList = UnorderedList()
-# print(myList.is_Empty())
+
+print("Is my list is empty? ",myList.is_Empty())
 myList.add(3)
 myList.add(31)
 myList.add(71)
 myList.add(10)
 myList.add(5)
 myList.add(1)
-# print(myList.is_Empty())
-# print("The index of the value is",myList.index(31))
-# print("Size of the list is",myList.listSize())
-# print(myList.search(10))
-# myList.delete(10)
-print("Size of the list is",myList.listSize())
+
+myList.display()
+print("The index of the value is",myList.index(31))
+
+print("Is my data is in the list? ",myList.search(71))
+myList.delete(71)
+print("Is my data is in the list? ",myList.search(71))
+
+myList.insert(2,99)
+print("Size of the list is now",myList.listSize())
 myList.display()
 myList.pop()
-print("Size of the list is",myList.listSize())
+print("Size of the list is now",myList.listSize())
+myList.display()
+myList.append(77)
+print("Size of the list is now",myList.listSize())
 myList.display()
