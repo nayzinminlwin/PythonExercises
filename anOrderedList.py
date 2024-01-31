3005229a2c179cda9c0b5012a5d824d4db7aa77f
Alex <nayzinminlwin22@gmail.com>
2024-01-31 14:01:41 +0630

    Finished implementing anOrderedList.py

--- anOrderedList.py
+++ anOrderedList.py
@@ -4,6 +4,19 @@ class OrderedList:
 	def __init__(self):
 		self.head = None
 
+	def is_Empty(self):
+		return self.head == None
+
+	def size(self):
+		count = 0
+		current = self.head
+		# while current.get_next()!=None:
+		while current != None:
+			count= count+1
+			current = current.get_next()
+
+		return count
+
 	def search(self,item):
 		current = self.head
 		found = False
@@ -28,16 +41,16 @@ class OrderedList:
 
 		while current!=None :
 			if current.get_data() >= item:
-				print("Going to break!!")
+				# print("Going to break!!")
 				break
-			print("Doing the two lines under!")
+			# print("Moving to next room!")
 			previous = current
 			current = current.get_next()
 
 		temp = Node(item)
 
 		if previous==None:
-			print("First room!")
+			# print("First room!")
 			temp.set_next(self.head)
 			self.head = temp
 		else:
@@ -57,6 +70,37 @@ class OrderedList:
 		previous.set_next(None)
 		return current.get_data()
 
+	def display(self):
+		current = self.head
+		print("The list is",end="")
+		while current!=None:
+			print(" ",current.get_data(),end="")
+			current = current.get_next()
+		print("")
+
+	def remove(self,item):
+		current = self.head
+		previous = None
+		found = False
+
+		while current!=None:
+			if item==current.get_data():
+				found =True
+				break
+			else :
+				previous = current
+				current = current.get_next()
+		if found:
+			if previous==None:
+				current.set_next(self.head)
+			else:
+				previous.set_next(current.get_next())
+			print(item,"is removed from the list!")
+		else:
+			print("The input was not found in array!")
+
+# Head->1->2->5->9->44->None
+
 myList = OrderedList()
 
 # print("Is my list is empty? ",myList.is_Empty())
@@ -66,6 +110,11 @@ myList.add(71)
 myList.add(10)
 myList.add(5)
 myList.add(1)
+print("Size of the list is",myList.size())
+myList.display()
 print("Now successfully popped",myList.pop())
-
-
+print("Size of the list is",myList.size())
+myList.display()
+myList.remove(10)
+print("Size of the list is",myList.size())
+myList.display()
