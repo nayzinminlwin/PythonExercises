a148a5b5ed9bcf69219b3bdace49df94dcfae832
Nay Zin <nayzinminlwin22@gmail.com>
2024-02-04 23:36:55 +0630

    Added ShellSort

---
+++ ShellSort.py
@@ -0,0 +1,17 @@
+def shellSort(array):
+	gap = len(array)//2
+
+	while gap>0:
+		for i in range(gap,len(array)):
+			val = array[i]
+			j = i
+			while j>=gap and array[j-gap]>val:
+				array[j] = array[j-gap]
+				j -=gap
+			array[j] = val
+
+		gap //=2
+	return array
+
+
+print(shellSort([5,6,4,7,12,9,1,8,32,49]))
