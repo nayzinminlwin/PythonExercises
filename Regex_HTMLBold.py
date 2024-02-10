0279a059884618cbf50ac7194000939130d24242
Nay Zin <nayzinminlwin22@gmail.com>
2024-02-10 12:29:05 +0630

    Added FindAll Function to Html Bold Test

--- Regex_HTMLBold.py
+++ Regex_HTMLBold.py
@@ -9,3 +9,11 @@ match = bold_regex.match(text)
 print(match)
 print(match.group(0))
 print(match.group(1))
+
+bold_regex1 = re.compile("<b>(.*?)</b>")
+
+text1 = "Hello <b>World</b>! This is <b>bold</b>."
+
+match1 = bold_regex1.findall(text1)
+print(match1)
+
