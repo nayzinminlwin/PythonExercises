70576b98d61e3dc7cffe23de4d439421db5ecbdf
Nay Zin <nayzinminlwin22@gmail.com>
2024-02-11 10:27:02 +0630

    Added bold and itlic funtion to mdToHtml.py

--- md_To_HTML.py
+++ md_To_HTML.py
@@ -8,3 +8,12 @@ print(replacement)
 text1 = "## Header 2"
 replacement1 = re.sub("^##\\s(.*)","<h2>\\1</h2>",text1)
 print(replacement1)
+
+text2 = "This is **bold**. This is another **bold** again."
+# replacement2 = re.sub("\*\*(.*?)\*\*","<b>\\1</b>",text2)
+replacement2 = re.sub("\\*\\*(.*?)\\*\\*","<b>\\1</b>",text2)
+print(replacement2)
+
+text3 = "This is _italic_.And this is another _italic_ again."
+replacement3 = re.sub("_(.*?)_","<i>\\1</i>",text3)
+print(replacement3)
