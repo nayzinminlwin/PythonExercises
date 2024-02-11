8c07274e0f8487e77f250fea3a4b69ebddc64b08
Nay Zin <nayzinminlwin22@gmail.com>
2024-02-11 10:17:42 +0630

    Continue Implementing mdToHtml.py

--- md_To_HTML.py
+++ md_To_HTML.py
@@ -1,6 +1,10 @@
 import re
 
-parser = re.compile("^#\s(.*)")
+parser = re.compile("^#\\s(.*)")
 text = "# Header 1"
 replacement = parser.sub("<h1>\\1</h1>",text)
 print(replacement)
+
+text1 = "## Header 2"
+replacement1 = re.sub("^##\\s(.*)","<h2>\\1</h2>",text1)
+print(replacement1)
