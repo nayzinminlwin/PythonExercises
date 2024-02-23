0ddf0e88edc69e270324a652175a3beba79fbf21
Nay Zin <nayzinminlwin22@gmail.com>
2024-02-23 23:50:49 +0630

    Upgrage the Exercise4 Q1 with Stack Class

--- FinalExercises/E4_Q1_divideBy8.py
+++ FinalExercises/E4_Q1_divideBy8.py
@@ -1,5 +1,24 @@
+class Stack:
+	def __init__(self):
+		self.item = []
+
+	def isEmpty(self):
+		return self.item == []
+
+	def push(self,newItem):
+		self.item.append(newItem)
+
+	def pop(self):
+		return self.item.pop()
+
+	def peek(self):
+		return self.item[len(self.item)-1]
+
+	def size(self):
+		return len(self.item)
+
 def divideBy8(BinaryNum):
-	result = []
+	result = Stack()
 	reminder = 0
 	exponent = BinaryNum
 
@@ -7,8 +26,12 @@ def divideBy8(BinaryNum):
 		reminder = int(exponent%8)
 		exponent = exponent/8;
 		# result[i] = reminder
-		result.insert(0,reminder)
-	return result
+		# result.insert(0,reminder)
+		result.push(reminder)
+	resultString = ""
+	while not result.isEmpty():
+		resultString = resultString + str(result.pop())
+	return resultString
 
 print(divideBy8(554))
-print(''.join(map(str,divideBy8(554))))
+# print(''.join(map(str,divideBy8(554))))
