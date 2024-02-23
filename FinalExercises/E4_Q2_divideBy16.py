071412dd22aa0dd4fd743332ad7e942ca03bb20b
Nay Zin <nayzinminlwin22@gmail.com>
2024-02-23 23:59:38 +0630

    Upgrade E4 Q2 with Stack Class

--- FinalExercises/E4_Q2_divideBy16.py
+++ FinalExercises/E4_Q2_divideBy16.py
@@ -1,5 +1,25 @@
+class Stack:
+	def __init__(self):
+		self.item = []
+
+	def isEmpty():
+		return self.item == []
+
+	def push(self,newItem):
+		self.item.append(newItem)
+
+	def pop(self):
+		return self.item.pop()
+
+	def peek(self):
+		return self.item[len(self.item)-1]
+
+	def size(self):
+		return len(self.item)
+
+
 def divideBy16(aDeciNum):
-	resultArr = []
+	resultArr = Stack()
 	exponent = aDeciNum
 	reminder = None
 
@@ -7,22 +27,33 @@ def divideBy16(aDeciNum):
 		reminder = exponent%16
 		exponent = exponent//16
 		if reminder<10:
-			resultArr.insert(0,reminder)
+			# resultArr.insert(0,reminder)
+			resultArr.push(reminder)
 		elif reminder==10:
-			resultArr.insert(0,"A")
+			# resultArr.insert(0,"A")
+			resultArr.push("A")
 		elif reminder==11:
-			resultArr.insert(0,"B")
+			# resultArr.insert(0,"B")
+			resultArr.push("B")
 		elif reminder==12:
-			resultArr.insert(0,"C")
+			# resultArr.insert(0,"C")
+			resultArr.push("C")
 		elif reminder==13:
-			resultArr.insert(0,"D")
+			# resultArr.insert(0,"D")
+			resultArr.push("D")
 		elif reminder==14:
-			resultArr.insert(0,"E")
+			# resultArr.insert(0,"E")
+			resultArr.push("E")
 		elif reminder==15:
-			resultArr.insert(0,"F")
+			# resultArr.insert(0,"F")
+			resultArr.push("F")
 		else:
-			resultArr.insert(0,"Hehehe")
+			# resultArr.insert(0,"Hehehe")
+			resultArr.push("Hehehe")
 
+	while not resultArr.isEmpty():
+		resultString = resultString + str(resultArr.pop())
 	return resultArr
 
-print("".join(map(str,divideBy16(668))))
+# print("".join(map(str,divideBy16(668))))
+
