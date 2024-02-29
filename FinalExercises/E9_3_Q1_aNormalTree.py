a81caabd3a59422484c982dfc473bc6cad6b05cf
Nay Zin <nayzinminlwin22@gmail.com>
2024-02-29 23:12:29 +0630

    Inserted Post and Pre Order Functions to Non Binary Tree

--- FinalExercises/E9_3_Q1_aNormalTree.py
+++ FinalExercises/E9_3_Q1_aNormalTree.py
@@ -6,12 +6,30 @@ class Node:
 	def get_rootValue(self):
 		return self.key
 
-	def addChild(self,name):
-		newChild = Node(name)
-		self.child.append(newChild)
+	def addChild(self,node):
+		self.child.append(node)
 
 	def getChild(self):
 		return self.child
+
+	def postOrder(self):
+		# print("Welcome to Preorder Method!")
+		if self!=None:
+			if(len(self.child)>0):
+				# for child in self.getChild():
+				# 	child.get_rootValue().postOrder()
+				for child in reversed(self.getChild()):
+					child.postOrder()
+
+			print(self.key)
+
+	def preOrder(self):
+		if self!=None:
+			print(self.key)
+			if(len(self.child)>0):
+				for child in self.getChild():
+					child.preOrder()
+
 
 not_Bi_Tree = Node("A")
 
@@ -19,11 +37,14 @@ b = Node("B")
 c = Node("C")
 d = Node("D")
 
-print(d.get_rootValue())
-
 not_Bi_Tree.addChild(b)
 not_Bi_Tree.addChild(c)
 not_Bi_Tree.addChild(d)
+
+# print(d.get_rootValue())
+# for x in not_Bi_Tree.child:
+# 	print(x.get_rootValue().get_rootValue())
+# 	# print(x)
 
 e= Node("E")
 f = Node("F")
@@ -33,6 +54,16 @@ b.addChild(e)
 b.addChild(f)
 b.addChild(g)
 
-for x in not_Bi_Tree.child:
-	print(x.get_rootValue().get_rootValue())
-	# print(x)
+h = Node("H")
+i = Node("I")
+
+c.addChild(h)
+c.addChild(i)
+
+j = Node("J")
+d.addChild(j)
+
+print("===========PostOrder===========")
+not_Bi_Tree.postOrder()
+print("==========PreOrder===========")
+not_Bi_Tree.preOrder()
